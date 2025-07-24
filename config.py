a173b703cdbe1a506831b59ca3a1e71568527646
Mrigakshi Kashyap <142392075+mrigakshikashyap@users.noreply.github.com>
2025-07-24 11:23:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 # Dummy config file containing fake secrets for secret scanning tests
 
 # AWS Keys 
-AWS_ACCESS_KEY_ID = "AKIAFAKE123456789"
-AWS_SECRET_ACCESS_KEY = "wJalrFakeSecretAccessKeyExample"
+AWS_ACCESS_KEY_ID = "AWS23456789"
+AWS_SECRET_ACCESS_KEY = "wJaSKJALKSKSJALKJSmamsals"
 
 # GitHub PAT 
-GITHUB_TOKEN = "ghp_fakePersonalAccessTokenExample123456789"
+GITHUB_TOKEN = "git123456789"
 
 # Slack Webhook (FAKE)
 SLACK_WEBHOOK = "https://hooks.slack.com/services/T00000000/B00000000/FAKEKEY123"
 
 # Google API Key 
-GOOGLE_API_KEY = "AIzaFakeGoogleAPIKey123456789"
+GOOGLE_API_KEY = "GoogleAPIKey123456789"
 
 # Database Credentials 
 DB_USER = "admin"
 DB_PASSWORD = "123"
 
 #Perplexity creds
-PERPLEXITY_API_KEY="ANJHDJHncncsjsh1234"
+PERPLEXITY_API_KEY="PERPANJHDJHncncsjsh1234"
