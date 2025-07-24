fff8206eb47ff0fd2023275bea8ce8cba526fdfe
Mrigakshi Kashyap <142392075+mrigakshikashyap@users.noreply.github.com>
2025-07-24 11:28:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,24 @@ import config
 import requests
 
 app = Flask(__name__)
+AWS_ACCESS_KEY_ID = "AWS23456789"
+AWS_SECRET_ACCESS_KEY = "wJaSKJALKSKSJALKJSmamsals"
+
+# GitHub PAT 
+GITHUB_TOKEN = "git123456789"
+
+# Slack Webhook (FAKE)
+SLACK_WEBHOOK = "https://hooks.slack.com/services/T00000000/B00000000/FAKEKEY123"
+
+# Google API Key 
+GOOGLE_API_KEY = "GoogleAPIKey123456789"
+
+# Database Credentials 
+DB_USER = "admin"
+DB_PASSWORD = "123"
+
+#Perplexity creds
+PERPLEXITY_API_KEY="PERPANJHDJHncncsjsh1234"
 
 @app.route("/")
 def home():
